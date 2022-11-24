423855d037fa4d2699b152d6e446857f713ab78b
duanxiang <duanxiang@cambricon.com>
2022-11-24 12:20:47 +0000

    delete useless file

--- download_model.sh
+++
@@ -1,1 +0,0 @@
-wget -c https://github.com/onnx/models/raw/main/vision/classification/resnet/model/resnet18-v1-7.onnx
--- gen_model.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ ! -f "bin/gen_model" ]; then
-    ./build.sh
-fi
-
-./bin/gen_model
